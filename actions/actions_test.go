93d0fbdbcfb9ebd0c7a4af13dde9d3bad42ce620
Brian Scott <brainscott@gmail.com>
2017-04-16 23:51:33 -0700

    init

---
+++ actions/actions_test.go
@@ -0,0 +1,17 @@
+package actions_test
+
+import (
+	"testing"
+
+	"github.com/bscott/golangflow/actions"
+	"github.com/gobuffalo/suite"
+)
+
+type ActionSuite struct {
+	*suite.Action
+}
+
+func Test_ActionSuite(t *testing.T) {
+	as := &ActionSuite{suite.NewAction(actions.App())}
+	suite.Run(t, as)
+}
